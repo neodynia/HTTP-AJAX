386370a2af61bf07cd391d27d3fc2eb2a0b4de80
ryanboris <ryan.boris@gmail.com>
2019-02-28 01:26:39 -0500

    :tada: initial commit on new branch

--- friends/src/AddFriend.js
+++ friends/src/AddFriend.js
@@ -2,39 +2,43 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-
 const FormContainer = styled.form`
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    height: 400px;
-    justify-content: space-evenly;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  height: 400px;
+  justify-content: space-evenly;
 
-    input[type='submit'] {
-        width: 100px;
-    }
-
+  input[type='submit'] {
+    width: 100px;
+  }
 `
 
-const AddFriend = (props) => {
-    return (
-        <FormContainer>
-            <label>
-                Name:
-                <input name="name" type="text" value={props.name} onChange={props.handleChange} />
-            </label>
-            <label>
-                Age:
-                <input name="age" type="text" value={props.age} onChange={props.handleChange} />
-            </label>
-            <label>
-                Email:
-                <input name="email" type="text" value={props.email} onChange={props.handleChange} />
-            </label>
-            <Link to='/'><input type="submit" value="Submit" onClick={props.handleSubmit} /></Link>
-        </FormContainer>
-    )
+const AddFriend = props => {
+  return (
+    <FormContainer>
+      <label>
+        Name:
+        <input name="name" type="text" value={props.name} onChange={props.handleChange} />
+      </label>
+      <label>
+        Age:
+        <input name="age" type="text" value={props.age} onChange={props.handleChange} />
+      </label>
+      <label>
+        Email:
+        <input
+          name="email"
+          type="text"
+          value={props.email}
+          onChange={props.handleChange}
+        />
+      </label>
+      <Link to="/">
+        <input type="submit" value="Submit" onClick={props.handleSubmit} />
+      </Link>
+    </FormContainer>
+  )
 }
-
 
 export default AddFriend
--- friends/src/App.js
+++ friends/src/App.js
@@ -7,91 +7,83 @@ import { Route } from 'react-router-dom'
 import UpdateFriend from './UpdateFriend'
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       friends: [],
       name: '',
       age: '',
       email: '',
-
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     axios
-            .get('http://localhost:5000/friends')
-            .then(response => this.setState({ friends: response.data }))
-            .catch(e => console.log(e))
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ friends: response.data }))
+      .catch(e => console.log(e))
   }
 
   handleChange = e => {
     const target = e.target
-    const value = target.name === 'age'
-            ? parseInt(target.value)
-            : target.value
+    const value = target.name === 'age' ? parseInt(target.value) : target.value
     const name = target.name
     this.setState({
-      [name]: value
+      [name]: value,
     })
   }
 
   handleSubmit = e => {
-  
     axios
-            .post('http://localhost:5000/friends', {
-              name: this.state.name,
-              age: this.state.age,
-              email: this.state.email,
- 
-            })
-            .then(response =>
-                this.setState({
-                  friends: response.data,
-                  id: '',
-                  name: '',
-                  age: '',
-                  email: '',
-             
-                })
-            )
-            .catch(e => console.log('ERROR', e))
+      .post('http://localhost:5000/friends', {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email,
+      })
+      .then(response =>
+        this.setState({
+          friends: response.data,
+          id: '',
+          name: '',
+          age: '',
+          email: '',
+        })
+      )
+      .catch(e => console.log('ERROR', e))
   }
 
   updateFriendForm = e => {
     this.setState({
-      redirect: !this.state.redirect
+      redirect: !this.state.redirect,
     })
   }
   updateFriend = e => {
-      axios
-        .put(`http://localhost:5000/friends/${e.target.id}`, {
-            name: this.state.name,
-            age: this.state.age,
-            email: this.state.email
-        })
-        .then(response => this.setState({friends: response.data}))
+    axios
+      .put(`http://localhost:5000/friends/${e.target.id}`, {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email,
+      })
+      .then(response => this.setState({ friends: response.data }))
   }
 
   deleteFriend = e => {
     axios
-            .delete(`http://localhost:5000/friends/${e.target.id}`)
-            .then(response =>
-                console.log(
-                    this.setState({ friends: response.data, name: '', age: '', email: '' })
-                )
-            )
-            .catch(e => console.log(e, 'ERROR'))
+      .delete(`http://localhost:5000/friends/${e.target.id}`)
+      .then(response =>
+        console.log(
+          this.setState({ friends: response.data, name: '', age: '', email: '' })
+        )
+      )
+      .catch(e => console.log(e, 'ERROR'))
   }
 
-
-
-  render () {
+  render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Route
           exact
-          path='/'
+          path="/"
           render={props => (
             <FriendList
               {...props}
@@ -99,12 +91,12 @@ class App extends Component {
               deleteFriend={this.deleteFriend}
               backToAdd={this.backToAdd}
               updateFriend={this.updateFriend}
-                        />
-                    )}
-                />
+            />
+          )}
+        />
         <Route
           exact
-          path='/addfriend'
+          path="/addfriend"
           render={props => (
             <AddFriend
               {...props}
@@ -113,13 +105,12 @@ class App extends Component {
               name={this.state.name}
               age={this.state.age}
               email={this.state.email}
-              
-                        />
-                    )}
-                />
+            />
+          )}
+        />
 
         <Route
-          path='/updatefriend'
+          path="/updatefriend"
           render={props => (
             <UpdateFriend
               {...props}
@@ -128,12 +119,9 @@ class App extends Component {
               name={this.state.name}
               age={this.state.age}
               email={this.state.email}
-              
-           
-                        />
-                    )}
-                />
-
+            />
+          )}
+        />
       </div>
     )
   }
